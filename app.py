bcbed7c73cf02df4c870fb051590727b574ec243
Ryan DeMilt <demilt.4@osu.edu>
2024-03-30 01:24:15 -0400

    undo tlb

--- app.py
+++ app.py
@@ -199,11 +199,11 @@ with tab_tlb:
     tlb_y = tlb_results['w']
     tlb_z = tlb_results['tlb']
 
-    fig = go.Figure()
+    # fig = go.Figure()
 
-    fig.add_trace(plotly_bar_charts_3d(tlb_x,tlb_y,tlb_z,color='x+y', x_title='Alphabet Size', y_title='Word Length',z_title= 'TLB'))
+    # fig.add_trace(plotly_bar_charts_3d(tlb_x,tlb_y,tlb_z,color='x+y', x_title='Alphabet Size', y_title='Word Length',z_title= 'TLB'))
 
-    # fig = plotly_bar_charts_3d(tlb_x,tlb_y,tlb_z,color='x+y', x_title='Alphabet Size', y_title='Word Length',z_title= 'TLB')
+    fig = plotly_bar_charts_3d(tlb_x,tlb_y,tlb_z,color='x+y', x_title='Alphabet Size', y_title='Word Length',z_title= 'TLB')
     st.plotly_chart(fig)
 
 
