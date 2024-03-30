68c9b1e15dc9bb7717fbf12b60bb248128c06f21
Ryan DeMilt <demilt.4@osu.edu>
2024-03-30 00:51:51 -0400

    fix

--- app.py
+++ app.py
@@ -182,7 +182,7 @@ with tab_tlb:
     tlb_results = tlb_results.replace('sfa','SFA')
     tlb_results = tlb_results.replace('spartan','SPARTAN')
 
-    tlb_results = tlb_results[tlb_results['method'] == tlb_family]
+    tlb_results = tlb_results[tlb_results['method'].isin(tlb_family)]
 
     tlb_x = tlb_results['a']
     tlb_y = tlb_results['w']
