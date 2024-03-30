ef3f4b97334b6444176a1db253dc7fbff5fd15c3
Ryan DeMilt <demilt.4@osu.edu>
2024-03-30 00:45:08 -0400

    import fix

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from st_aggrid import AgGrid
 import plotly.graph_objects as go
 from statistical_test import graph_ranks
 
-from .barchart import plotly_bar_charts_3d
+from barchart import plotly_bar_charts_3d
 
 st. set_page_config(layout="wide") 
 st.set_option('deprecation.showPyplotGlobalUse', False)
